2d42e59b8d7668782b9e2443bf49976fd0393f9d
keg-git <keg.git@proton.me>
2025-11-20 22:46:19 -0500

    no idea

--- keyboards/sofle_choc/keymaps/KEG/keymap.c
+++ keyboards/sofle_choc/keymaps/KEG/keymap.c
@@ -102,29 +102,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_SYM] = LAYOUT(
-        KC_GRV,KC_1,            KC_2,            KC_3,            KC_4,    KC_5,                             KC_6,            KC_7,              KC_8,          KC_9,    KC_0,    _______,
-        KC_LALT,KC_HASH,         KC_SLSH,         KC_LBRC,         KC_RBRC, KC_PLUS,                          KC_PIPE,           KC_LT,           KC_GT,         KC_BSLS, KC_QUOT, KC_CIRC,
-        KC_LCTL,  KC_AT,         KC_ASTR,         KC_LPRN,         KC_RPRN,  KC_EQL,                          KC_AMPR,         KC_SCLN,         KC_PDOT,         KC_COMM, KC_DQUO, KC_TILD,
-        KC_LSFT, KC_DLR,         KC_PERC,         KC_LCBR,         KC_RCBR, KC_MINS,  _______,       _______, KC_QUES,         KC_COLN,         KC_EXLM,         KC_UNDS,  KC_GRV, _______,
-                                 KC_MUTE,         _______,         KC_BSPC,  KC_SPC, MO(_NAV),       _______, KC_LSFT,           KC_F6,           KC_F7,           KC_F8
+        KC_GRV,KC_1,      KC_2,    KC_3,    KC_4,    KC_5,                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+        KC_LALT,KC_HASH,  KC_SLSH, KC_LBRC, KC_RBRC, KC_PLUS,                          KC_PIPE, KC_LT,   KC_GT,   KC_BSLS, KC_QUOT, KC_CIRC,
+        KC_LCTL,  KC_AT,  KC_ASTR, KC_LPRN, KC_RPRN, KC_EQL,                           KC_AMPR, KC_SCLN, KC_PDOT, KC_COMM, KC_DQUO, KC_TILD,
+        KC_LSFT, KC_DLR,  KC_PERC, KC_LCBR, KC_RCBR, KC_MINS,  _______,       _______, KC_QUES, KC_COLN, KC_EXLM, KC_UNDS, KC_GRV,  _______,
+                          KC_MUTE, _______, KC_BSPC, KC_SPC,  MO(_NAV),       _______, KC_LSFT, KC_F6,   KC_F7,   KC_F8
         ),
 
 // Game Layers
 // Base Game Layout great for destiny
 [_GAME] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,     KC_3,    KC_4,   KC_5,                          KC_6,        KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
-    KC_TAB,  KC_Q,   KC_W,     KC_E,    KC_R,   KC_T,                          KC_Y,     KC_U, KC_I, KC_O,    KC_P,  KC_EQL,
-    KC_LCTL, KC_A,   KC_S,     KC_D,    KC_F,   KC_G,                          KC_H,     KC_J, KC_K, KC_L, KC_SCLN,  KC_QUOT,
+    KC_ESC,  KC_1,   KC_2,     KC_3,    KC_4,   KC_5,                          KC_6,     KC_7, KC_8, KC_9, KC_0,    KC_BSPC,
+    KC_TAB,  KC_Q,   KC_W,     KC_E,    KC_R,   KC_T,                          KC_Y,     KC_U, KC_I, KC_O, KC_P,    KC_EQL,
+    KC_LCTL, KC_A,   KC_S,     KC_D,    KC_F,   KC_G,                          KC_H,     KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,   KC_X,     KC_C,    KC_V,   KC_B, _______,     _______,    KC_N,        KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
                      KC_M,   KC_O, MO(_GAMEH), KC_LALT, KC_SPC,    KC_SPC, TG(_GAME), LALT(KC_F4),  KC_F12,  KC_F11
 ),
 
 [_GAMEH] = LAYOUT(
-  KC_ESC,   KC_F1,   KC_F2,     KC_F3,    KC_F4,   KC_F5,                          KC_F6,        KC_F7,    KC_F8,    KC_F9,    KC_0,  KC_MINS,
-    KC_TAB,  KC_CTLTAB,   KC_UP,     KC_ALTAB,    KC_R,   KC_T,                          KC_Y,     KC_BTN5, KC_MS_U, KC_BTN4,    KC_P,  KC_EQL,
-    KC_LCTL, KC_LEFT,   KC_DOWN, KC_RGHT,    KC_J,   KC_G,                          KC_B,     KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN2,  KC_QUOT,
-    KC_LSFT, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, _______,     _______,    KC_N,        KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
-                     KC_M,   KC_O, _______, KC_LALT, KC_SPC,    TG(_GAME), KC_BTN1, LALT(KC_F4),  KC_F12,  KC_F11
+    KC_ESC,  KC_F1,     KC_F2,      KC_F3,      KC_F4,   KC_F5,                         KC_F6,   KC_F7,       KC_F8,   KC_F9,   KC_0,    KC_MINS,
+    KC_TAB,  KC_CTLTAB, KC_UP,      KC_ALTAB,   KC_R,    KC_T,                          KC_Y,    KC_BTN5,     KC_MS_U, KC_BTN4, KC_P,    KC_EQL,
+    KC_LCTL, KC_LEFT,   KC_DOWN,    KC_RGHT,    KC_J,    KC_G,                          KC_B,    KC_MS_L,     KC_MS_D, KC_MS_R, KC_BTN2, KC_QUOT,
+    KC_LSFT, KC_Y,      KC_U,       KC_I,       KC_O,    KC_P,    _______,     _______, KC_N,    KC_M,        KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+                        LGUI(KC_D), LGUI(KC_M), _______, KC_LALT, KC_SPC,    TG(_GAME), KC_BTN1, LALT(KC_F4), KC_F12,  KC_F11
 ),
 /* ADJUST  not correct
  * ,-----------------------------------------.                    ,-----------------------------------------.
