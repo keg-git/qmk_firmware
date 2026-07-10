8c727c7365702b59a68587404c4bd038a62c3504
keg-git <keg.git@proton.me>
2026-07-10 17:26:41 -0400

    stuff need to pull from upstream

--- keyboards/sofle_choc/keymaps/KEG/keymap.c
+++ keyboards/sofle_choc/keymaps/KEG/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS,
-  KC_LALT,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_EQL,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_EQL,
   KC_LCTL,  LGUI_T(KC_A),LALT_T(KC_S),LCTL_T(KC_D),LSFT_T(KC_F), KC_G,            KC_H,RSFT_T(KC_J),RCTL_T(KC_K),RALT_T(KC_L),  LGUI_T(KC_SCLN),  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MPLY,     KC_MUTE, KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
                  KC_LGUI,KC_F4,TG(_GAME),KC_SPC, LT(_NAV,KC_BSPC),  LT(_SYM,KC_SPC), MO(_NUM), KC_DEL, KC_F12, KC_F11
@@ -122,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_GAMEH] = LAYOUT(
     KC_ESC,  KC_F1,     KC_F2,      KC_F3,      KC_F4,   KC_F5,                         KC_F6,   KC_F7,       KC_F8,   KC_F9,   KC_0,    KC_MINS,
     KC_TAB,  KC_CTLTAB, KC_UP,      KC_ALTAB,   KC_R,    KC_T,                          KC_Y,    MS_BTN5,     MS_UP,   MS_BTN4, KC_P,    KC_EQL,
-    KC_LCTL, KC_LEFT,   KC_DOWN,    KC_RGHT,    KC_J,    KC_G,                          KC_B,    MS_LEFT,     MS_DOWN, MS_RGHT,MS_BTN2, KC_QUOT,
+    LGUI(KC_X), KC_LEFT,   KC_DOWN,    KC_RGHT,    KC_J,    KC_G,                          KC_B,    MS_LEFT,     MS_DOWN, MS_RGHT,MS_BTN2, KC_QUOT,
     KC_LSFT, KC_Y,      KC_U,       KC_I,       KC_O,    KC_P,    _______,     _______, KC_N,    KC_M,        KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
                         LGUI(KC_D), LGUI(KC_M), _______, KC_LALT, KC_SPC,    TG(_GAME), MS_BTN1, LALT(KC_F4), KC_F12,  KC_F11
 ),
